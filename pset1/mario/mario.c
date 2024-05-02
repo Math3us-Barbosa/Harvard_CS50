dd21ca0d7a66074233c4beb18e4abb0a0d984e4e
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-05-02 17:59:28 +0000

    Thu, May 2, 2024, 2:59 PM -03:00

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -3,8 +3,8 @@
 
  int main(void){
     //declaraçao de variaveis
-    int n,c=1;
-    int x=0;
+    int n, c = 1;
+    int x = 0;
     //perguntando ao usuario o tamanho da piramide e garantindo que esse seja entre 1 e 8
     do{
         printf("type the pyramid height between 1 and 8\n");
@@ -12,20 +12,20 @@
         if(n>=1 && n<=8)
             x=1;
         }
-        while(x!=1);
-    while(c<=n){
+        while(x! = 1);
+    while(c <= n){
         //printando o espaço entre o começo da linha e os # da respectiva linha
-        for (int j=0;j<(n-c);j++){
+        for (int j = 0; j < (n - c); j++){
             printf(" ");
         }
     //printando os # da primeira piramide
-        for (int i=q0;i<c;i++){
+        for (int i = 0; i < c; i++){
             printf("#");
         }
     //espaço entre as piramides
         printf("  ");
     //loop para printar os # da segunda piramide
-        for (int k = 0;k < c;k++){
+        for (int k = 0; k < c; k++){
             printf("#");
         }
      // somando um no contador
