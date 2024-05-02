3fae0600e947ed4e6ba7a198a00eda99f5995c70
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-05-02 18:49:08 +0000

    Thu, May 2, 2024, 3:49 PM -03:00

---
+++ pset1/mario_less/mario.c
@@ -0,0 +1,21 @@
+nt main(void){
+    //declaraçao de variaveis
+    int n, c = 1;
+    int x = 0;
+    //perguntando ao usuario o tamanho da piramide e garantindo que esse seja entre 1 e 8
+    do{
+        printf("type the pyramid height between 1 and 8\n");
+        scanf("%i",&n);
+        if(n>=1 && n<=8)
+            x=1;
+        }
+        while(x! = 1);
+        //printando o espaço entre o começo da linha e os # da respectiva linha
+        for (int j = 0; j < (n - c); j++){
+            printf(" ");
+        }
+    //printando os # da primeira piramide
+        for (int i = 0; i < c; i++){
+            printf("#");
+        }
+    }
