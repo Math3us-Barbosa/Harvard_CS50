dc821a59435a9a42ed28cd50480c25d7f721059f
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-05-07 14:37:29 +0000

    Tue, May 7, 2024, 11:37 AM -03:00

---
+++ pset2/readability/readability.c
@@ -0,0 +1,45 @@
+int main(void)
+{
+    //declairing variables
+    int letters = 0, sentences = 0;
+    float words = 1;
+
+
+    string T = get_string("text: ");
+    //loop to analyse each character of the string until it's over
+    for (int i = 0, n = strlen(T); i < n; i++)
+    {
+        if (isalpha(T[i]))
+        {
+            letters++;
+        }
+        else if (isspace(T[i]))
+        {
+            words++;
+        }
+        else if (T[i] == '.' || T[i] == '!' || T[i] == '?')
+        {
+            sentences++;
+        }
+
+
+    }
+    //calculating the  average number of Letters(L) and Sentences(S) per 100 words
+    float L = (letters / words) * 100;
+    float S = (sentences / words) * 100;
+    //calculating the index by the formula
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+    // showing the results
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
