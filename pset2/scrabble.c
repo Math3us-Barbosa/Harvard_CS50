3bace3dc1154569d4bccea574f6df68676a95f0e
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-05-24 14:26:19 +0000

    Fri, May 24, 2024, 11:26 AM -03:00

--- pset2/scrabble.c
+++ pset2/scrabble.c
@@ -17,16 +17,18 @@ int main(void)
     // Score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
-    int max1 = strlen(score1);
-    int max2 = strlen(score2);
-    for(int i = 0; i < max1; i++)
-    {
-        
-    }
     // TODO: Print the winner
 }
 
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
+    int max = strlen(word);
+    for (int i = 0; i < max; i++)
+    {
+        if(isalpha(word[i]))
+        {
+            
+        }
+
+    }
 }
