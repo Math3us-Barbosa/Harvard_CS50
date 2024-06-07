209f9397c7981f5a22074caa7c9df7a96ab74a7e
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-06-07 15:52:07 +0000

    Fri, Jun 7, 2024, 12:52 PM -03:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -20,9 +20,11 @@ int main(int argc, char *argv[])
     char *d filename = malloc(4);
     do{
         fread(buffer,sizeof(BYTE),512,f);
-        if(buffer[0] == 0Xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-
-    }while(!f.eof);
+        } while(buffer[0] != 0Xff || buffer[1] != 0xd8 || buffer[2] != 0xff || (buffer[3] & 0xf0) != 0xe0);
+    while(!f.eof)
+    {
+        printf
+    }
     fclose(f);
     fclose(d);
     free(d);
