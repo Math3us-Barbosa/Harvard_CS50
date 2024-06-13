b3ad9e6b916b5b28c521ca6afb08cb8cfee6d014
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-06-13 23:03:29 +0000

    Thu, Jun 13, 2024, 8:03 PM -03:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -245,9 +245,12 @@ void print_winner(void)
 bool iscircle(int x)
 {
     int cont = 0;
-    for(int i = 0; i < candidate_count ; i++)
+    for(int j = 0; j < candidate_count ; j++)
     {
-        for
+        for(i = 0; i < candidate_count; i++)
+        {
+            if locked
+        }
 
 
     }
