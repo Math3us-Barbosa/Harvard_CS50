2fa5235c14b03c6d737566e0bb9424b5b7a29be2
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-06-13 13:34:34 +0000

    Thu, Jun 13, 2024, 10:34 AM -03:00

--- pset4/lab4/volume.c
+++ pset4/lab4/volume.c
@@ -18,14 +18,14 @@ int main(int argc, char *argv[])
     }
 
     // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "rb");
+    FILE *input = fopen(argv[1], "r");
     if (input == NULL)
     {
         printf("Could not open file.\n");
         return 1;
     }
 
-    FILE *output = fopen(argv[2], "wb");
+    FILE *output = fopen(argv[2], "w");
     if (output == NULL)
     {
         printf("Could not open file.\n");
