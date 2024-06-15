290fecd6077a03bf652e2c0959a6dc3540c40a4c
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-06-15 16:23:42 +0000

    Sat, Jun 15, 2024, 1:23 PM -03:00

--- mergesort.c
+++ mergesort.c
@@ -2,17 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void mergesort(int*v);
+
 int main(void)
 {
     int max;
     srand(time(NULL));
-    printf("digite o tamanho do vetor\n");
+    printf("type de size of the array\n");
     scanf("%i"&max);
+    printf("unsorted array:  ");
     int *v = malloc(20*sizeof(int));
     for(int i = 0; int i < max; i++)
     {
-        v[i] = rand()%100
+        v[i] = rand()%100;
+        printf("%i",v[i]);
     }
+    printf("\n");
+    printf("array ordenado: ");
+    mergesort(v);
+}
+
+void mergesort(int*v)
+{
+    if(sizeof(v) == 1)
+    {
+        return
+    }
+    int v1 = malloc
 
 
 }
