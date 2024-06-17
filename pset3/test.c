1fc50eb81dd603c730f4aaf74909b0cf5e1b3fbd
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-06-17 15:31:02 +0000

    Mon, Jun 17, 2024, 12:31 PM -03:00

--- pset3/test.c
+++ pset3/test.c
@@ -29,7 +29,7 @@ int main(void)
 void merge(int *v, int left, int mid, int right)
 {
     int max = right - left +1 , p1 = left, p2 = mid + 1;
-    int *temp = malloc(max*sizeof(int));
+    int* temp = malloc(max*sizeof(int));
     for(int i =0; i <max; i++)
     {
         if(v[p1] < v[p2])
