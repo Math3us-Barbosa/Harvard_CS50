5583e658648497131855399fbd757efaf2547a84
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-06-26 17:47:02 +0000

    Wed, Jun 26, 2024, 2:47 PM -03:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -105,6 +105,10 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
+    node *tmp;
+    for(int = 0; i < N;i++)
+    {
+
+    }
     return false;
 }
