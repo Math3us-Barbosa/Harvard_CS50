e8e189d512b51557cd5996c306b80518f6358b61
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-07-08 16:59:42 +0000

    Mon, Jul 8, 2024, 1:59 PM -03:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -79,38 +79,35 @@ bool load(const char *dictionary)
     }
     while((ch = fgetc(f)) != EOF)
     {
-        for (i = 0; (i < 45 && ((ch = fgetc(stream)) != EOF) && (ch != '\n')); i++)
+        bool word_loaded = false;
+        int i = 0;
+        node *n = malloc(sizeof(node));
+        if(n == NULL)
         {
-            printf("%s\n",s);
-            bool word_loaded = false;
-            int i = 0;
-            node *n = malloc(sizeof(node));
-            if(n == NULL)
-            {
-                printf("out of memory \n");
-                return false;
-            }
-            printf("%c",n->word[i]);
-            strcpy(n-> word,s);
-            printf("%s",n->word);
-            n-> next = NULL;
-            index = hash(s);
-            for(node *tmp = table[index];i < 1;tmp = tmp->next)
-            {
-                if(tmp == NULL)
-                {
-                    tmp = n;
-                    i++;
-                    siz++;
-                    word_loaded = true;
-                }
-            }
-            if(word_loaded == false)
-            {
-                return false;
-            }
-
+            printf("out of memory \n");
+            return false;
         }
+        printf("%c",n->word[i]);
+        strcpy(n-> word,s);
+        printf("%s",n->word);
+        n-> next = NULL;
+        index = hash(s);
+        for(node *tmp = table[index];i < 1;tmp = tmp->next)
+        {
+            if(tmp == NULL)
+            {
+                tmp = n;
+                i++;
+                siz++;
+                word_loaded = true;
+            }
+        }
+        if(word_loaded == false)
+        {
+            return false;
+        }
+
+
     }
     loaded = 0;
     free(s);
