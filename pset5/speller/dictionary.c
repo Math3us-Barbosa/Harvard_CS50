0b7ebca9eec87a7a960e0f9bf5938f34bee65041
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-07-09 12:01:44 +0000

    Tue, Jul 9, 2024, 9:01 AM -03:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -76,9 +76,9 @@ bool load(const char *dictionary)
         printf("file %s not found",dictionary);
         return false;
     }
-    while((ch = fgetc(f)) != EOF)
+    while(fread(&ch,1,1,f) != NULL)
     {
-        pri
+        printf("%c"ch);
         int j = 0;
         int i = 0;
         bool word_loaded = false;
