f4d12adc8048b8d7d41f97a8417ddbdbdc34ea43
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-08-05 21:59:08 +0000

    Mon, Aug 5, 2024, 6:59 PM -03:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -38,7 +38,7 @@ bool check(const char *word)
     {
         if(isalpha(word[i]))
             s[i] = tolower(word[i]);
-        else if(word[i] == 39)
+        else if(word[i] == 39 && isalpha(word[i-1]) && isalpha(word[i+1]))
             {
             s[i] = word[i];
             test = 0;
