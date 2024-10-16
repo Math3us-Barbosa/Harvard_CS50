83ba66409d97aef03e3de5a0c10cad4c6bef6177
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-10-16 17:29:18 +0000

    Wed, Oct 16, 2024, 2:29 PM -03:00

--- pset6/mario_hard/cash.py
+++ pset6/mario_hard/cash.py
@@ -3,4 +3,16 @@ from cs50 import get_float
 change = get_float("Change owed: ")
 coins = 0
 while change > 0
-    if change> 0.25
+    if change >= 0.25
+        coins += 1
+        change -= 0.25
+    elif change >= 0.10
+        coins +=1
+        change -= 0.10
+    elif change >= 0.5
+        coins += 1
+        change -= 0.10
+    elif change >= 0.1
+        coins += 1
+        change -=0.1
+print(coins)
