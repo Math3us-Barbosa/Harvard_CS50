1408087f95210f8e79e6b6fe826b109c3632456d
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-10-16 17:35:58 +0000

    Wed, Oct 16, 2024, 2:35 PM -03:00

--- pset6/cash.py
+++ pset6/cash.py
@@ -3,7 +3,7 @@ from cs50 import get_float
 change = get_float("Change owed: ")
 print(change)
 coins = 0
-while roundchange > 0.0:
+while change > 0.0:
     if change >= 0.25:
         coins += 1
         change -= 0.2500000
