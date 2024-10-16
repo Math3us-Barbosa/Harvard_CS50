dbf027aabd5461dc310a94fb371d32b18e799398
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-10-16 16:34:42 +0000

    Wed, Oct 16, 2024, 1:34 PM -03:00

--- pset6/mario.py
+++ pset6/mario.py
@@ -1,9 +1,6 @@
 from cs50 import get_int
 
 height = get_int("Height: ")
-while i < height:
-    i += 1
-    j = 0
-    while j < i:
-        print("#", end: "")
+for i in range (height):
+    printf("#")
 print()
