8e7737aecc87fd3e19134126425fa12f5a28bcee
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-10-16 17:13:53 +0000

    Wed, Oct 16, 2024, 2:13 PM -03:00

--- pset6/mario_hard/mario.py
+++ pset6/mario_hard/mario.py
@@ -1,8 +1,13 @@
 from cs50 import get_int
 
 height = 0
-while heigh < 1 or height > 8
+while height < 1 or height > 8:
     height = get_int("Height: ")
-    if height < 1 or height > 8
+    if height < 1 or height > 8:
         print("height vallue must be between 1 and 8")
+for i in range (height):
+    print(" " * (height - 1 - i), end = "")
+    print("#" * i, end = " ")
+    print("#" * i, end = "")
+    print(" " *(height - 1 - i))
 
