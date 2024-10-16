e4f0e9361226d642c00a49be03da324b0fb80a25
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-10-16 17:32:38 +0000

    Wed, Oct 16, 2024, 2:32 PM -03:00

--- pset6/cash.py
+++ pset6/cash.py
@@ -1,18 +1,20 @@
 from cs50 import get_float
 
 change = get_float("Change owed: ")
+print(change)
 coins = 0
-while change > 0
-    if change >= 0.25
+while change > 0.0:
+    if change >= 0.25:
         coins += 1
         change -= 0.25
-    elif change >= 0.10
+        print(change)
+    elif change >= 0.10:
         coins +=1
         change -= 0.10
-    elif change >= 0.5
+    elif change >= 0.5:
         coins += 1
         change -= 0.10
-    elif change >= 0.1
+    elif change >= 0.1:
         coins += 1
         change -=0.1
 print(coins)
