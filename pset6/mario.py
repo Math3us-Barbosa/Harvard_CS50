efa93961a3ed27dda4908a24eaa60a24446bc276
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-10-16 16:58:24 +0000

    Wed, Oct 16, 2024, 1:58 PM -03:00

--- pset6/mario.py
+++ pset6/mario.py
@@ -1,5 +1,5 @@
 from cs50 import get_int
-height = int(input("Height: "))
+height = get_int("Height: ")
 for i in  range (height):
     print(" " * (height - 1 - i),end = "")
     print("#" * (i+1))
