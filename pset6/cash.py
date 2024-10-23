aea0743572ebfce93ef00cfb94ab3434cc4addb7
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-10-23 17:17:11 +0000

    Wed, Oct 23, 2024, 2:17 PM -03:00

--- pset6/cash.py
+++ pset6/cash.py
@@ -1,25 +1,29 @@
 from cs50 import get_float
 
-#getting change vallue
+# Getting change value from user
 change = get_float("Change owed: ")
-#counter for coins started as 0
+
+# Counter for coins starts at 0
 coins = 0
-#while change > 0.00 chose 
-while round(change,2) > 0:
-    if change >= 0.25:
+
+# Loop while there's still change to give, rounding to 2 decimal places
+while round(change, 2) > 0:
+    if change >= 0.25:  # If change is at least 0.25, give a quarter
         coins += 1
         change -= 0.25
-        change = round(change,2)
-    elif change >= 0.10:
-        coins +=1
+        change = round(change, 2)
+    elif change >= 0.10:  # If change is at least 0.10, give a dime
+        coins += 1
         change -= 0.10
-        change = round(change,2)
-    elif change >= 0.05:
+        change = round(change, 2)
+    elif change >= 0.05:  # If change is at least 0.05, give a nickel
         coins += 1
         change -= 0.05
-        change = round(change,2)
-    elif change >= 0.01:
+        change = round(change, 2)
+    elif change >= 0.01:  # If change is at least 0.01, give a penny
         coins += 1
-        change -=0.01
-        change = round(change,2)
+        change -= 0.01
+        change = round(change, 2)
+
+# Print the total number of coins used
 print(coins)
