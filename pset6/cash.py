38f49d90f16c5369b752af24d619a1fa49b9fd0a
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-10-23 17:10:11 +0000

    Wed, Oct 23, 2024, 2:10 PM -03:00

--- pset6/cash.py
+++ pset6/cash.py
@@ -7,17 +7,17 @@ while round(change,2) > 0:
     if change >= 0.25:
         coins += 1
         change -= 0.25
-    change = round(change,2)
+        change = round(change,2)
     elif change >= 0.10:
         coins +=1
         change -= 0.10
-    change = round(change,2)
+        change = round(change,2)
     elif change >= 0.05:
         coins += 1
         change -= 0.05
-    change = round(change,2)
+        change = round(change,2)
     elif change >= 0.1:
         coins += 1
         change -=0.01
-    change = round(change,2)
+        change = round(change,2)
 print(coins)
