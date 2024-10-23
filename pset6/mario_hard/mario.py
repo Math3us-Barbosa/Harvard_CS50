48d1c1f26d10e66a232ae0c623fd75abe4486b78
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-10-23 17:51:39 +0000

    Wed, Oct 23, 2024, 2:51 PM -03:00

--- pset6/mario_hard/mario.py
+++ pset6/mario_hard/mario.py
@@ -7,7 +7,7 @@ while height < 1 or height > 8:
         print("height vallue must be between 1 and 8")
 for i in range (height):
     print(" " * (height - 1 - i), end = "")
-    print("#" * (i + 1), end = " ")
+    print("#" * (i + 1), end = "  ")
     print("#" * (i + 1), end = "")
     print(" " *(height - 1 - i))
 
