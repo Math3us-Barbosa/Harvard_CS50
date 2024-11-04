7d2032c901505dd16e6341be3f78c9e8be52824a
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-04 19:24:13 +0000

    Mon, Nov 4, 2024, 4:24 PM -03:00

--- pset6/credit/credit.py
+++ pset6/credit/credit.py
@@ -1,19 +1,19 @@
 import re
 
-n = get_int("Number: ")
+num = get_int("Number: ")
 c = 0
 x = 0
 y = 0
-//loop para checar validade do numero
+#loop to check if the number is valid or not
 while n > 0:
-    if c % 2 != 0: //se impar
+    if c % 2 != 0: #if odd
         y = 2 * (n % 10)
         if y >= 10:
             x = x + (y % 10) + (y / 10)
         else:
             x = x + y
         n = n / 10
-    elif c % 2 == 0 //se par
+    elif c % 2 == 0 #if even
         x = x + (n % 10)
         n = n / 10
 
@@ -21,11 +21,10 @@ while n > 0:
     if n < 100 and n > 10:
         p = n
     c+=
-num = str(n)
-patterns = {
-    "AMEX": r"^3[47][0-9]{13}$",          # Começa com 34 ou 37, e tem 15 dígitos
-    "VISA": r"^4[0-9]{12}(?:[0-9]{3})?$", # Começa com 4, e tem 13 ou 16 dígitos
-    "MASTERCARD": r"^5[1-5][0-9]{14}$"    # Começa com 51-55, e tem 16 dígitos
-}
-if re.match(pattern,num)
-    print(pattern)
+if x % 10 != 0:
+    print("INVALID")
+elif c = 15 and (p = 34 or p = 37):
+    print("AMEX")
+elif c = 16 
+
+
