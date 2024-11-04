bd4790c6a675d1137b69293f9138e7fb6238fb78
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-04 18:40:13 +0000

    Mon, Nov 4, 2024, 3:40 PM -03:00

--- pset6/credit/credit.py
+++ pset6/credit/credit.py
@@ -1,2 +1,7 @@
 import re
 
+patterns = {
+    "AMEX": r"^3[47][0-9]{13}$",          # Começa com 34 ou 37, e tem 15 dígitos
+    "VISA": r"^4[0-9]{12}(?:[0-9]{3})?$", # Começa com 4, e tem 13 ou 16 dígitos
+    "MASTERCARD": r"^5[1-5][0-9]{14}$"    # Começa com 51-55, e tem 16 dígitos
+}
