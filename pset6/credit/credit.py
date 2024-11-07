de455998a0270d973f6276bc21e6fe790f3cc54e
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-07 14:29:32 +0000

    Thu, Nov 7, 2024, 11:29 AM -03:00

--- pset6/credit/credit.py
+++ pset6/credit/credit.py
@@ -9,22 +9,24 @@ while n > 0:
     if c % 2 != 0: #if odd
         y = 2 * (n % 10)
         if y >= 10:
-            x = x + (y % 10) + (y / 10)
+            x = x + (y % 10) + int((y / 10))
         else:
             x = x + y
         n = n / 10
     elif c % 2 == 0 #if even
         x = x + (n % 10)
-        n = n / 10
+        n = int(n / 10)
 
     #declairing first 2 digits as p
     if n < 100 and n > 10:
         p = n
-    c+=
+    c+= 1
 if x % 10 != 0:
     print("INVALID")
 elif c = 15 and (p = 34 or p = 37):
     print("AMEX")
-elif c = 16 and p
-
+elif (c = 16 or c = 13) and int(p/10) == 4
+    print("VISA")
+else
+    print("MASTERCARD")
 
