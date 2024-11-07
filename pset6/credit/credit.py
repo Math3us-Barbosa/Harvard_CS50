a3773484d25505fa942bcb97e9df761c7fc307f5
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-07 14:41:33 +0000

    Thu, Nov 7, 2024, 11:41 AM -03:00

--- pset6/credit/credit.py
+++ pset6/credit/credit.py
@@ -9,15 +9,10 @@ y = 0
 while n > 0:
     if c % 2 != 0: #if odd
         y = 2 * (n % 10)
-        if y >= 10:
-            x = x + (y % 10) + int((y / 10))
-        else:
-            x = x + y
-        n = n / 10
+        x = x + (y % 10) + int((y / 10))
     elif c % 2 == 0: #if even
         x = x + (n % 10)
-        n = int(n / 10)
-
+    n = int(n / 10)
     #declairing first 2 digits as p
     if n < 100 and n > 10:
         p = n
