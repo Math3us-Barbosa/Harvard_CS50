2d8ce116eff6e5eb61f9674a53835b9ee4b43c03
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-11 19:34:31 +0000

    Mon, Nov 11, 2024, 4:34 PM -03:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -10,7 +10,8 @@ def main():
         sys.exit(1)
     # TODO: Read database file into a variable
     with open(sys.argv[1],"r") as file:
-        reader = csv.DictReader(file)
+        reader = csv.reader(file)
+        for row in reader
         sequences = reader.fieldnames
     # TODO: Read DNA sequence file into a variable
     with open (sys.argv[2],"r") as f:
