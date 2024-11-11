ceace7906c15ab3c5fa25ccaf1e06c51ea3beac3
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-11 15:04:39 +0000

    Mon, Nov 11, 2024, 12:04 PM -03:00

--- pset6/readability/readability.py
+++ pset6/readability/readability.py
@@ -2,7 +2,10 @@ from cs50 import get_string
 
 text = get_string("Text: ")
 max = strlen(text)
-word_counter  =  1
-for i in range (max)
-    if(text)
-    text[i]
+word_counter =  0
+letter_counter = 0
+for i in range (max):
+    if text[i] == ' ' or text[i] == NULL:
+        word_counter  +=  1
+    elif text[i]
+        letter coun
