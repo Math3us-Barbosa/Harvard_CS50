ad7a18975513cfa419374bf61c44a42ccc42db2c
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-11 16:05:51 +0000

    Mon, Nov 11, 2024, 1:05 PM -03:00

--- pset6/readability/readability.py
+++ pset6/readability/readability.py
@@ -1,23 +1,25 @@
 from cs50 import get_string
-
+#getting text from the user
 text = get_string("Text: ")
+#declairing variables
 N = len(text)
-word_counter = 1
+word_counter = 1 # start as one to count last word
 letter_counter = 0
 sentences = 0
+#loop to iterate through the string
 for i in range (N):
-    if text[i].isspace():
+    if text[i].isspace():  #space indicate end of the word
         word_counter  +=  1
-    elif text[i].isalpha():
+    elif text[i].isalpha(): #con
         letter_counter += 1
     elif text[i] == "." or text[i]  == "!" or text[i] == "?":
         sentences += 1
-print(word_counter)
-print(letter_counter)
-print(sentences)
+#calculating L and S
 L = (letter_counter/word_counter) * 100
 S = (sentences/word_counter) * 100
+#calculating index by the Coleman-Liau formula
 index = round(0.0588 * L - 0.296 * S - 15.8)
+#printing the results
 if(index > 16):
     print("Grade 16+")
 elif(index < 1):
