1fb36450506c35c2b1b909fc752eb92720bf444a
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-11 23:16:17 +0000

    Mon, Nov 11, 2024, 8:16 PM -03:00

--- pset6/lab6/tournament.py
+++ pset6/lab6/tournament.py
@@ -25,7 +25,7 @@ def main():
         counts[row["name"]] = 0
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range N
-        simulate_tournament(teams)
+        counts[simulate_tournament(teams)] += 1
     # Print each team 's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
@@ -37,7 +37,7 @@ def simulate_game(team1, team2):
     rating2 = team2["rating"]
     probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
     return random.random() < probability
-    
+
 
 def simulate_round(teams):
     """Simulate a round. Return a list of winning teams."""
