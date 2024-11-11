871a1db2fc5c5b47278dbef9abd0ea573b50bdfc
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-11 16:12:47 +0000

    Mon, Nov 11, 2024, 1:12 PM -03:00

--- pset6/mario/mario.py
+++ pset6/mario/mario.py
@@ -1,10 +1,12 @@
 from cs50 import get_int
 height = 0
+#getting the height from the user, the height must be between 1 and 8
 while height < 1 or height > 8:
     height = get_int("Height: ")
     if height < 1 or height > 8:
         print("height vallue must be between 1 and 8")
+#loop to iterate through all lines
 for i in  range (height):
-    print(" " * (height - 1 - i),end = "")
-    print("#" * (i + 1))
+    print(" " * (height - 1 - i),end = "") #printing spaces before the pyramdide
+    print("#" * (i + 1)) #printing hashes from the pyramide
 
--- pset6/mario_hard/mario.py
+++ pset6/mario_hard/mario.py
@@ -1,14 +1,16 @@
 from cs50 import get_int
 
 height = 0
+#getting the height from the user, the height must be between 1 and 8
 while height < 1 or height > 8:
     height = get_int("Height: ")
     if height < 1 or height > 8:
         print("height vallue must be between 1 and 8")
+#loop to iterate through all lines
 for i in range (height):
-    print(" " * (height - 1 - i), end = "")
-    print("#" * (i + 1), end = "  ")
-    print("#" * (i + 1), end = "")
-    print("")
+    print(" " * (height - 1 - i), end = "") #printing spaces before 1st pyramide
+    print("#" * (i + 1), end = "  ") #printing hashes from 1st pyramide/space at the end
+    print("#" * (i + 1), end = "")  #printing hashes from 2nd pyramide
+    print("") #new line
 
 
