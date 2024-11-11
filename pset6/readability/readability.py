53e7cffae1e72b999a9c8b51473f7fc9b6aaad96
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-11 16:34:29 +0000

    Mon, Nov 11, 2024, 1:34 PM -03:00

--- pset6/readability/readability.py
+++ pset6/readability/readability.py
@@ -3,16 +3,16 @@ from cs50 import get_string
 text = get_string("Text: ")
 # declairing variables
 N = len(text)
-word_counter = 1 # start as one to count last word
+word_counter = 1  # start as one to count last word
 letter_counter = 0
 sentences = 0
 # loop to iterate through the string
 for i in range(N):
-    if text[i].isspace():  #space indicate end of the word
-        word_counter  +=  1
+    if text[i].isspace():  # space indicate end of the word
+        word_counter += 1
     elif text[i].isalpha():
         letter_counter += 1
-    elif text[i] == "." or text[i]  == "!" or text[i] == "?":
+    elif text[i] == "." or text[i] == "!" or text[i] == "?":
         sentences += 1
 # calculating L and S
 L = (letter_counter/word_counter) * 100
@@ -20,10 +20,9 @@ S = (sentences/word_counter) * 100
 #  calculating index by the Coleman-Liau formula
 index = round(0.0588 * L - 0.296 * S - 15.8)
 # printing the results
-if(index > 16):
+if index > 16:
     print("Grade 16+")
-elif(index < 1):
+elif index < 1:
     print("Before Grade 1")
 else:
     print("Grade " + str(index))
-
