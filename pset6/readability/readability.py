97c1ec239a90f6edce9dc4b4fac0cf08ecfbd382
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-11 15:39:04 +0000

    Mon, Nov 11, 2024, 12:39 PM -03:00

--- pset6/readability/readability.py
+++ pset6/readability/readability.py
@@ -8,7 +8,7 @@ sentences = 0
 for i in range (N):
     if text[i].isspace():
         word_counter  +=  1
-    elif text[i].isalpha:
+    elif text[i].isalpha():
         letter_counter += 1
     elif text[i] == "." or text[i]  == "!" or text[i] == "?":
         sentences += 1
@@ -17,5 +17,5 @@ print(letter_counter)
 print(sentences)
 L = (letter_counter/word_counter) * 100
 S = (sentences/word_counter) * 100
-index = int(0.0588 * L - 0.296 * S - 15.8)
+index = round(0.0588 * L - 0.296 * S - 15.8)
 print("Grade: " + str(index))
