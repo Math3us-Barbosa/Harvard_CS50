a37558d48acec208c9d96965c2bf29e3098b8f94
Math3us-Barbosa <167922157+Math3us-Barbosa@users.noreply.github.com>
2024-11-11 15:36:18 +0000

    Mon, Nov 11, 2024, 12:36 PM -03:00

--- pset6/readability/readability.py
+++ pset6/readability/readability.py
@@ -12,6 +12,9 @@ for i in range (N):
         letter_counter += 1
     elif text[i] == '.' or text[i]  == '!' or text[i] == "?":
         sentences += 1
+print(word_counter)
+print(letter_counter)
+print(sentences)
 L = (letter_counter/word_counter) * 100
 S = (sentences/word_counter) * 100
 index = int(0.0588 * L - 0.296 * S - 15.8)
